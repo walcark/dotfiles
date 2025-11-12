34093d10d73fc8a45fa1b06e590498bd81db79ad
walcark <kevin.walcarius@cnes.fr>
2025-11-12 17:36:12 +0100

    Update wezterm config file with no-top-bar and toyonight theme.

--- private_dot_config/wezterm.lua
+++ private_dot_config/wezterm.lua
@@ -1,18 +1,25 @@
 local wezterm = require 'wezterm'
 
-return {
-  -- --- Appearance ---
-  color_scheme = "Builtin Solarized Dark",  -- theme intégré
-  font = wezterm.font("JetBrainsMono Nerd Font"),  -- police lisible
-  font_size = 11.0,
+local config = {}
 
-  -- --- Window ---
-  window_background_opacity = 0.95,  -- légère transparence
-  enable_tab_bar = false,            -- pas de barre d’onglets
-  window_padding = {
-    left = 5, right = 5, top = 5, bottom = 5,
-  },
+-- Global appearance --
+config.color_scheme = 'Tokyo Night Storm'
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 12.0
 
-  -- --- Behavior ---
-  default_prog = {"/bin/bash"},      -- ou "zsh", "fish", etc.
+-- Window --
+config.window_background_opacity = 0.95
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+config.enable_scroll_bar = true
+config.window_padding = {
+    left = 5, 
+	right = 5, 
+	top = 5, 
+	bottom = 5,
 }
+
+-- Shell --
+config.default_prog = {"/bin/bash"}
+
+return config
