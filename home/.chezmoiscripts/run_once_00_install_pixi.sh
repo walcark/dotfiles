e273e2890e7d72343efaf7553fff75c374307028
walcark <kevin.walcarius@cnes.fr>
2025-11-25 10:34:34 +0100

    feat: improve pixi downloading with a backup script.

--- home/.chezmoiscripts/run_once_00_install_pixi.sh
+++ home/.chezmoiscripts/run_once_00_install_pixi.sh
@@ -1,16 +1,37 @@
 #!/usr/bin/env bash
 set -e
 
-
+# Test pixi installation
 if command -v pixi >/dev/null 2>&1; then
     echo "[chezmoi] Pixi already installed"
     exit 0
 fi
-
 echo "[chezmoi] Installing Pixi..."
 
-# Installation officielle
-curl -fsSL https://pixi.sh/install.sh | bash
+# Set installer location
+TMP_SCRIPT="/tmp/install_pixi.sh"
+FALLBACK_SCRIPT="$HOME/.local/share/chezmoi/install/common/pixi_backup.sh"
+
+# Installing pixi
+if curl -fsSL https://pixi.sh/install.sh -o "$TMP_SCRIPT"; then
+  echo "[chezmoi] Install pixi from curled file."
+  bash "$TMP_SCRIPT"
+else
+  echo "[chezmoi] Failed to download from pixi.sh (blocked or unreachable)."
+  echo "[chezmoi] Trying fallback installer."
+
+  if [[ -f "$FALLBACK_SCRIPT" ]]; then
+    bash "$FALLBACK_SCRIPT"
+  else
+    echo "[chezmoi] ERROR: fallback script not found: $FALLBACK_SCRIPT"
+    exit 1
+  fi 
+fi 
+
+echo "[chezmoi] Pixi installation completed."
+
+# Add Pixi to path
+export PATH="$HOME/"
 
 # Add pixi to path
 export PATH="$HOME/.pixi/bin:$PATH"
