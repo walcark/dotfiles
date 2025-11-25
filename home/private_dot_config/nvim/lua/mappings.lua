56f9e65fb5a6324e7f1ad971251b80e8abc79a32
walcark <kevin.walcarius@cnes.fr>
2025-11-25 16:01:37 +0100

    feat: change <space>ff keymap to telescope dot files (except .git).

--- home/private_dot_config/nvim/lua/mappings.lua
+++ home/private_dot_config/nvim/lua/mappings.lua
@@ -3,8 +3,9 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local default_opts = {noremap = true}
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map('n', '<leader>ff', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", default_opts)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
