710a7f58965cbb31119450b36264fc750f5e8bbb
walcark <kevin.walcarius@cnes.fr>
2026-02-11 12:06:32 +0100

    feat: merge astrocommunity.pack.python into a single local config file.

--- home/private_dot_config/nvim-astro/lua/pack/python.lua
+++ home/private_dot_config/nvim-astro/lua/pack/python.lua
@@ -1,39 +1,119 @@
+-- File to define my custom python LSP servers
+-- 1. Pyright : typing
+-- 2. Ruff    : linting, fixing, formatting
+
 return {
-  { import = "astrocommunity.pack.python" },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruff" })
-    end,
-  },
   {
     "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
     opts = {
+      ---@diagnostic disable: missing-fields
       config = {
-        ruff = { on_attach = function(client) client.server_capabilities.hoverProvider = false end },
+        basedpyright = {
+          before_init = function(_, c)
+            if not c.settings then c.settings = {} end
+            if not c.settings.python then c.settings.python = {} end
+            c.settings.python.pythonPath = vim.fn.exepath "python"
+          end,
+          settings = {
+            basedpyright = {
+              -- See: https://docs.basedpyright.com/latest/configuration/language-server-settings/
+              -- See: https://docs.basedpyright.com/latest/configuration/config-files/#overriding-language-server-settings
+              analysis = {
+                typeCheckingMode = "recommended",
+                autoImportCompletions = true,
+              },
+            },
+          }
+        },
+        ruff = {
+          -- See: https://docs.astral.sh/ruff/editors/settings/#format_backend
+          on_attach = function(client)
+            client.server_capabilities.hoverProvider = false
+          end,
+          configurationPreference = "filesystemFirst",
+          lineLength = 79,
+          organizeImports = true,
+          showSyntaxErrors = true
+        },
       },
     },
   },
   {
-    "jay-babu/mason-null-ls.nvim",
+    "nvim-treesitter/nvim-treesitter",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = vim.tbl_filter(
-        function(v) return not vim.tbl_contains({ "black", "isort" }, v) end,
-        opts.ensure_installed
-      )
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "python", "toml" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "basedpyright", "ruff" })
+    end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "python" })
+      if not opts.handlers then opts.handlers = {} end
+      opts.handlers.python = function() end -- make sure python doesn't get set up by mason-nvim-dap, it's being set up by nvim-dap-python
     end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "ruff" })
-      opts.ensure_installed = vim.tbl_filter(
-        function(v) return not vim.tbl_contains({ "black", "isort" }, v) end,
-        opts.ensure_installed
-      )
+      opts.ensure_installed =
+          require("astrocore").list_insert_unique(opts.ensure_installed, { "basedpyright", "debugpy" })
+    end,
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    enabled = vim.fn.executable "fd" == 1 or vim.fn.executable "fdfind" == 1 or vim.fn.executable "fd-find" == 1,
+    dependencies = {
+      {
+        "AstroNvim/astrocore",
+        opts = {
+          mappings = {
+            n = {
+              ["<Leader>lv"] = { "<Cmd>VenvSelect<CR>", desc = "Select VirtualEnv" },
+            },
+          },
+        },
+      },
+    },
+    opts = {},
+    cmd = "VenvSelect",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    specs = {
+      {
+        "mfussenegger/nvim-dap-python",
+        dependencies = "mfussenegger/nvim-dap",
+        ft = "python", -- NOTE: ft: lazy-load on filetype
+        config = function(_, opts)
+          local path = vim.fn.exepath "debugpy-adapter"
+          if path == "" then path = vim.fn.exepath "python" end
+          require("dap-python").setup(path, opts)
+        end,
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = { "nvim-neotest/neotest-python", config = function() end },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-python" (require("astrocore").plugin_opts "neotest-python"))
     end,
   },
   {
