52f6ca974ebc8c9bdbb9f9785a69ae6e2f53f49a
walcark <kevin.walcarius@cnes.fr>
2026-02-11 14:10:49 +0100

    config: add alias for beautiful git log in .gitconfig.

---
+++ home/dot_bashrc.d/10-alias_navigation.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+# Global navigation aliases
+# -----------------------------------------------
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias ll='ls -lh --color=auto'
+alias la='ls -lAh --color=auto'
+alias c='clear'
+alias h='history | tail -50'
+alias mkdir='mkdir -p'
+alias rm='rm -i'
+alias ..='cd ..'
+alias ...='cd ../..'
+
+
+# Home subdirs navigation aliases
+# -----------------------------------------------
+alias lib="cd ${HOME}/library"
+alias tosort="cd ${HOME}/library/to-sort"
+alias docs="cd ${HOME}/docs"
+alias notes="cd ${HOME}/zk/notes"
+alias sand="cd ${HOME}/dev/sandbox"
+alias curp="cd ${HOME}/dev/current"
+alias fork="cd ${HOME}/dev/forks"
+alias loc='cd ~/.local'
+alias conf='cd ~/.config'
