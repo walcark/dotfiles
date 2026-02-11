d24305b5103a06e43e54eb10a00bb12bc030389c
walcark <kevin.walcarius@cnes.fr>
2026-02-11 09:12:40 +0100

    fix: add TERM enforcing for trex connection on kitty.

--- home/bin/executable_trex-connect.sh
+++ home/bin/executable_trex-connect.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 keepassxc-cli clip ~/Sync/pswd/mdps_keepass.kdbx "Server/Trex"
 sleep 0.2
 echo "Password for Trex was copied to clipboard for 15 seconds..."
-ssh -X walcark@trex.cnes.fr
+TERM=xterm-256color ssh -X walcark@trex.cnes.fr
 sleep 15
 xclip -selection clipboard /dev/null
--- home/dot_bashrc.d/11-bin_exports-and-aliases.sh
+++ home/dot_bashrc.d/11-bin_exports-and-aliases.sh
@@ -7,5 +7,8 @@ export PATH="${BIN_PATH}/keepassxc:${PATH}"
 # Export path for configurations files
 export STARSHIP_CONFIG="$HOME/.config/starship.toml"
 
+# Export enviromnent variables for specific libs 
+export MOPSMAP_DATASET_PATH="/home/kwalcarius/bin/mopsmap/optical_dataset"
+
 # Add alias for some binaries
 alias kpxc="keepassxc-cli"
