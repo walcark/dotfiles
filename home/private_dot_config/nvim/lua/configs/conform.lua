067bf2439c611336008e296ce7cf0e017140cea4
walcark <kevin.walcarius@cnes.fr>
2026-05-12 15:44:43 +0200

    del: remove used nvchad config.

--- home/private_dot_config/nvim/lua/configs/conform.lua
+++
@@ -1,9 +0,0 @@
-local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
-    yaml = { "yamlfmt" },
-  },
-}
-
-return options
--- home/private_dot_config/nvim/lua/configs/lspconfig.lua
+++
@@ -1,9 +0,0 @@
-require("nvchad.configs.lspconfig").defaults()
-
-local servers = { "pylsp" }
-
-vim.lsp.enable(servers)
-
-
-
--- read :h vim.lsp.config for changing options of lsp servers
